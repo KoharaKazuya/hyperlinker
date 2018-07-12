e2cf20b2e198b0c35c160325833a0e83cac783ff
KoharaKazuya <reflect.results@gmail.com>
2018-07-12 19:24:59 +0900

    ignore DOMException

--- src/hyperlinker/deactivate.js
+++ src/hyperlinker/deactivate.js
@@ -4,8 +4,17 @@
  */
 const deactivate = smashed => {
   for (const s of smashed) {
-    s.parent.insertBefore(s.original, s.replaced[0]);
-    for (const r of s.replaced) s.parent.removeChild(r);
+    try {
+      s.parent.insertBefore(s.original, s.replaced[0]);
+      for (const r of s.replaced) s.parent.removeChild(r);
+    } catch (e) {
+      // 置き換え時点での DOM 構造と異なる可能性があるため、
+      // DOMException は無視する
+      // 置き換えた DOM 構造を完全に元に戻すことは諦める
+      if (!(e instanceof DOMException)) {
+        throw e;
+      }
+    }
   }
 };
 
