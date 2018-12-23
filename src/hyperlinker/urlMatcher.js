ddc1a67002a65923d2dc1bd4635783c8763bca6c
KoharaKazuya <reflect.results@gmail.com>
2018-12-23 13:11:56 +0900

    new urlMatcher along with RFC3986

--- src/hyperlinker/urlMatcher.js
+++ src/hyperlinker/urlMatcher.js
@@ -1,6 +1,10 @@
 /**
  * URL (ハイパーリンク) としてみなせる文字列にマッチする正規表現
+ *
+ * @see [RFC 3986](https://tools.ietf.org/html/rfc3986) を参考に (厳密ではない)
+ *
+ * - マークダウンのリンクなどの末尾が `)` で終わる文字列は末尾の部分をマッチさせない
  */
-const urlMatcher = /https?:\/\/((?![ "#<>`{}])[\u0020-\u007e])+/;
+const urlMatcher = /(?:[A-Z][A-Z0-9+.-]+)?:\/\/(?:(?!\)\s)[A-Z0-9._~%!$&'()*+,;=:@/?#-])+/i;
 
 export default urlMatcher;
